1d1727469bffa8ed2a3d9b5df8bc1106bc6b2b73
DmitryNotebook <microlab471@gmail.com>
2021-12-24 03:01:07 +0300

    Add Player

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    private float inputHorizontal;
+    
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        playerRigidbody.AddForce(new Vector3(inputHorizontal,0f,0f),ForceMode.Acceleration);
+    }
+}
