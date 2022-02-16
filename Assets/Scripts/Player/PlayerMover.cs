a4e8e2171a44d74a50fc68d6e0426a3d07642285
Dima <Microlab471@gmail.com>
2022-02-16 23:29:09 +0300

    edit scripts

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerMover : MonoBehaviour
 {
